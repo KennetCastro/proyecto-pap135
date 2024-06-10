1a4f797b1490e893e841022df3d1174faf92e392
KennetCastro <cs24010@ues.edu.sv>
2024-06-09 18:30:22 -0600

    Añade constructor y método de acceso

--- src/servicios/BancoDB.java
+++ src/servicios/BancoDB.java
@@ -1,10 +1,18 @@
 package servicios;
 import java.util.HashMap;
+import modelos.Cuenta;
 
 public class BancoDB {
 	// Hashmap de String hasta que se creen las clases correspondientes
 	
 	private static HashMap<Integer, String> clientes = new HashMap<Integer, String>();
-	private static HashMap<Integer, String> cuentas = new HashMap<Integer, String>();
+	private static HashMap<Integer, Cuenta> cuentas = new HashMap<Integer, Cuenta>();
 	private static HashMap<Integer, String> transacciones = new HashMap<Integer, String>();
+	
+	public BancoDB() {}
+	
+	public HashMap<Integer, Cuenta> getCuentas() {
+		return cuentas;
+	}
+
 }
