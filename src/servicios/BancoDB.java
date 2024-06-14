3ffe7ba623a415bbe07a77f9d3e632fdf8319bc9
KennetCastro <cs24010@ues.edu.sv>
2024-06-13 20:34:10 -0600

    Añade método de acceso y cambia tipo de dato

--- src/servicios/BancoDB.java
+++ src/servicios/BancoDB.java
@@ -1,5 +1,7 @@
 package servicios;
 import java.util.HashMap;
+import java.util.List;
+
 import modelos.*;
 
 public class BancoDB {
@@ -7,7 +9,7 @@ public class BancoDB {
 	
 	private static HashMap<Integer, Cliente> clientes = new HashMap<Integer, Cliente>();
 	private static HashMap<Integer, Cuenta> cuentas = new HashMap<Integer, Cuenta>();
-	private static HashMap<Integer, String> transacciones = new HashMap<Integer, String>();
+	private static HashMap<Integer, List<Transaccion>> transacciones = new HashMap<Integer, List<Transaccion>>();
 	
 	public BancoDB() {}
 	
@@ -19,4 +21,7 @@ public class BancoDB {
 		return clientes;
 	}
 
+	public static HashMap<Integer, List<Transaccion>> getTransacciones() {
+		return transacciones;
+	}
 }
