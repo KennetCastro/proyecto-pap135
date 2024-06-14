d4f24c08e559aa03cd7e27c82607fe834c25ca0b
KennetCastro <cs24010@ues.edu.sv>
2024-06-13 20:37:53 -0600

    Arregla tipos de datos y nombre de método

--- src/servicios/GestorDeTransacciones.java
+++ src/servicios/GestorDeTransacciones.java
@@ -3,6 +3,9 @@ package servicios;
 import java.util.ArrayList;
 import java.util.List;
 
+import modelos.Cuenta;
+import modelos.Transaccion;
+
 public class GestorDeTransacciones {
     private List<Transaccion> historialTransacciones;
 
@@ -10,32 +13,32 @@ public class GestorDeTransacciones {
         historialTransacciones = new ArrayList<>();
     }
 
-    public void registrarDeposito(CuentaBancaria cuenta, double monto) {
+    public void registrarDeposito(Cuenta cuenta, double monto) {
         cuenta.depositar(monto);
         historialTransacciones.add(new Transaccion(monto, null, cuenta));
-        System.out.println("Depósito registrado: " + monto + " a la cuenta " + cuenta.getNumeroCuenta());
+        System.out.println("Depósito registrado: " + monto + " a la cuenta " + cuenta.getNumCuenta());
     }
 
-    public void registrarRetiro(CuentaBancaria cuenta, double monto) {
+    public void registrarRetiro(Cuenta cuenta, double monto) {
         cuenta.retirar(monto);
         historialTransacciones.add(new Transaccion(monto, cuenta, null));
-        System.out.println("Retiro registrado: " + monto + " de la cuenta " + cuenta.getNumeroCuenta());
+        System.out.println("Retiro registrado: " + monto + " de la cuenta " + cuenta.getNumCuenta());
     }
 
-    public void registrarTransferencia(CuentaBancaria cuentaOrigen, CuentaBancaria cuentaDestino, double monto) {
+    public void registrarTransferencia(Cuenta cuentaOrigen, Cuenta cuentaDestino, double monto) {
         cuentaOrigen.transferir(monto, cuentaDestino);
         historialTransacciones.add(new Transaccion(monto, cuentaOrigen, cuentaDestino));
-        System.out.println("Transferencia registrada: " + monto + " de la cuenta " + cuentaOrigen.getNumeroCuenta() + " a la cuenta " + cuentaDestino.getNumeroCuenta());
+        System.out.println("Transferencia registrada: " + monto + " de la cuenta " + cuentaOrigen.getNumCuenta() + " a la cuenta " + cuentaDestino.getNumCuenta());
     }
 
     public void mostrarHistorial() {
         for (Transaccion t : historialTransacciones) {
             if (t.getCuentaOrigen() == null) {
-                System.out.println("Depósito de " + t.getMonto() + " a la cuenta " + t.getCuentaDestino().getNumeroCuenta());
+                System.out.println("Depósito de " + t.getMonto() + " a la cuenta " + t.getCuentaDestino().getNumCuenta());
             } else if (t.getCuentaDestino() == null) {
-                System.out.println("Retiro de " + t.getMonto() + " de la cuenta " + t.getCuentaOrigen().getNumeroCuenta());
+                System.out.println("Retiro de " + t.getMonto() + " de la cuenta " + t.getCuentaOrigen().getNumCuenta());
             } else {
-                System.out.println("Transferencia de " + t.getMonto() + " de la cuenta " + t.getCuentaOrigen().getNumeroCuenta() + " a la cuenta " + t.getCuentaDestino().getNumeroCuenta());
+                System.out.println("Transferencia de " + t.getMonto() + " de la cuenta " + t.getCuentaOrigen().getNumCuenta() + " a la cuenta " + t.getCuentaDestino().getNumCuenta());
             }
         }
     }
